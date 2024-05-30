2c834d1a295f3c278cc5919acb05f21716daaf31
tharuAnish <namoanishtharu@gmail.com>
2024-05-30 17:49:06 +0530

    contact page created

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,160 @@
-import React from "react"
+"use client"
 
-export default function ContactPage() {
-  return <div>ContactPage</div>
+import Link from "next/link"
+import { FC, useState } from "react"
+import { FaBuilding, FaMapMarkerAlt, FaEnvelope } from "react-icons/fa"
+
+const Contact: FC = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    country: "United States",
+    language: "English (US)",
+    message: "",
+  })
+
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
+    const { name, value } = e.target
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }))
+  }
+
+  return (
+    <main className="pt-14 max-w-5xl mx-auto">
+      <h1 className="font-bold text-4xl pb-11">ContactUs Today</h1>
+      <section
+        style={{ boxShadow: "2px 5px 22px #0003" }}
+        className=" shadow-xl rounded-xl items-center p-10 h-full  flex flex-wrap max-w-5xl mx-auto"
+      >
+        <section className="h-full text-sm flex px-10 gap-5 flex-col basis-2/4 ">
+          <div className="flex flex-col border  rounded-lg p-4 w-full items-center  md:mb-0 text-center">
+            <FaBuilding size={32} />
+            <p className="mt-2">Company information:</p>
+            <p>Anish Tharu</p>
+            <p>Tax id: USXXXXX</p>
+          </div>
+          <div className="flex flex-col text-sm p-4 w-full  mx-auto rounded-lg border items-center  md:mb-0 text-center">
+            <FaMapMarkerAlt size={32} />
+            <p className="mt-2">Address:</p>
+            <p>Los angeles, United States</p>
+            <p>1941 Late Avenue</p>
+            <p>Zip Code/Postal code: 560097</p>
+          </div>
+          <div className="flex flex-col  mx-auto p-4 rounded-lg border items-center text-center">
+            <FaEnvelope size={32} />
+            <p className="mt-2">Contact us:</p>
+            <p className="">
+              Email us for general queries, including marketing and partnership.
+            </p>
+            <p>tharuanish.vercel.com</p>
+          </div>
+        </section>
+        <section className="basis-2/4   ">
+          <form className="space-y-6 border text-sm p-5 rounded-lg">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <input
+                type="text"
+                name="firstName"
+                placeholder="First name"
+                value={formData.firstName}
+                onChange={handleChange}
+                className="p-2 rounded  w-full border"
+              />
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Last name"
+                value={formData.lastName}
+                onChange={handleChange}
+                className="p-2 rounded  w-full border"
+              />
+            </div>
+            <input
+              type="email"
+              name="email"
+              placeholder="Your email"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-full p-2 rounded border"
+            />
+            <input
+              type="text"
+              name="phoneNumber"
+              placeholder="Phone number"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              className="w-full p-2 rounded border"
+            />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <select
+                name="country"
+                value={formData.country}
+                onChange={handleChange}
+                className="p-2 rounded  w-full border"
+              >
+                <option value="United States">India</option>
+                <option value="United States">United States</option>
+                <option value="United States">Canada</option>
+                <option value="United States">Russia</option>
+              </select>
+              <select
+                name="language"
+                value={formData.language}
+                onChange={handleChange}
+                className="p-2 rounded  w-full border"
+              >
+                <option value="English (US)">English (US)</option>
+                <option value="English (US)">English (UK)</option>
+                <option value="English (US)">Spanish </option>
+                <option value="English (US)">Hindi</option>
+              </select>
+            </div>
+            <textarea
+              name="message"
+              placeholder="Your message"
+              rows={6}
+              value={formData.message}
+              onChange={handleChange}
+              className="w-full p-2 rounded border"
+            />
+            <div className="flex items-center">
+              <input type="checkbox" name="terms" className="mr-2" />
+              <p>
+                I confirm that you have read and agreed to{" "}
+                <Link href="#" className="underline text-blue-400">
+                  Terms of Service.
+                </Link>
+              </p>
+            </div>
+            <button
+              type="submit"
+              className="w-full p-2 bg-blue-600 rounded text-white hover:bg-blue-700"
+            >
+              Send message
+            </button>
+          </form>
+        </section>
+      </section>
+      <section className="my-20 w-full">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3890.0214385207382!2d77.664129!3d12.8418911!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae6c86072ac00f%3A0xa9f83d961b00093a!2sVenkateswara%20Technopark%2C%202nd%20Cross%20Rd%2C%20Electronics%20City%20Phase%201%2C%20Electronic%20City%2C%20Bengaluru%2C%20Karnataka%20560100!5e0!3m2!1sen!2sin!4v1717068741695!5m2!1sen!2sin"
+          height={450}
+          className="w-full"
+          allowFullScreen={true}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </section>
+    </main>
+  )
 }
+
+export default Contact
