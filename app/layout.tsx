190c49fdab153f913dfdd2ceb118afdb551b26b5
tharuAnish <namoanishtharu@gmail.com>
2024-06-01 23:57:37 +0530

    added meta data for better SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,14 @@ import "./globals.css"
 import Navbar from "@/components/nav/navbar"
 import { ThemeProvider } from "@/components/theme/themeProvider"
 import Footer from "@/components/footer/footer"
+import Head from "next/head"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Blog Page",
-  description: "This is a blog page",
+  title: "Namo's Blog",
+  description:
+    "Welcome to Namo's Blog, a space where Anish Tharu shares insights on technology, coding, programming, and more.",
 }
 
 export default function RootLayout({
@@ -19,6 +21,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="author" content="Anish Tharu" />
+        <meta
+          name="description"
+          content="Welcome to Namo's Blog, a space where Anish Tharu shares insights on technology, coding, programming, and more."
+        />
+        <meta
+          name="keywords"
+          content="Namo's Blog, tech blog, coding, programming, Anish Tharu, technology insights"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="Namo's Blog" />
+        <meta
+          property="og:description"
+          content="Welcome to Namo's Blog, a space where Anish Tharu shares insights on technology, coding, programming, and more."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://anishblog.vercel.app/" />
+        <meta
+          property="og:image"
+          content="https://anishblog.vercel.app/og-image.jpg"
+        />
+        <title>Namo's Blog</title>
+      </Head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
